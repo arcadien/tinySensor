0e071458b265eb4d807d147ff2c25fbaa4c0a9cb
Aurelien Labrosse <aurelien@skima.fr>
2018-12-09 22:51:57 +0100

    Fix bug in LED pin

--- config.h
+++ config.h
@@ -17,7 +17,7 @@
 /*
  * 1-wire interface is on PA3
  * Power for radio is on PA2 (sensor_vcc)
- * LED pin is PB3
+ * LED pin is PB1
  * PWM pin is PA7
  * SDA pin is PA6
  * SCL pin is PA4
@@ -26,23 +26,23 @@
  * BAT sensor pin is PA1
  * analog sensor pin is PA0
  */
-#define LED_PIN PB3
+#define LED_PIN PB1
 #define PWM_PIN PA7
 #define TX_RADIO_PIN PB0
 #define RADIO_POWER_PIN PA2
 #define BAT_SENSOR_PIN PA1
 
 // Sensors
-//#define USE_BMP280
+#define USE_BMP280
 //#define USE_BME280
-#define USE_DS18B20 1
+//#define USE_DS18B20 
 // Protocols
 #define USE_OREGON 1
 
 // 0x5A, 0x5D pressure, temp, hygro
 // 0xEA, 0x4C temp only
 const uint8_t OREGON_TYPE[] = {0xEA, 0x4C};
-const uint8_t OREGON_ID = 0xCA;
+const uint8_t OREGON_ID = 0xCB;
 
 /*
  * MODE_0 0 // Temperature only [THN132N]
