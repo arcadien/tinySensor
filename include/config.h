8d0498419ab1c205358d16b5894f4227044b0554
Aurelien Labrosse <aurelien@skima.fr>
2020-01-07 23:32:53 +0100

    Add license comments in source files

--- include/config.h
+++ include/config.h
@@ -1,8 +1,19 @@
 /*
- * config.h
+ * This file is part of the TinySensor distribution (https://github.com/arcadien/TinySensor)
  *
- * Created: 19/11/2018 22:14:40
- *  Author: aurelien
+ * Copyright (c) 2019 Aurélien Labrosse
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 /*
-* TinySensor.cpp
-*
-* Created: 02/10/2018 21:23:28
-* Author : aurelien
-*/
+ * This file is part of the TinySensor distribution (https://github.com/arcadien/TinySensor)
+ *
+ * Copyright (c) 2019 Aurélien Labrosse
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <TinySensor.h>
 #include <config.h>
