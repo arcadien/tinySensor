36d0d87288e3fa8d55bf88bfb47a872c28b07bc1
Aurelien Labrosse <aurelien@skima.fr>
2020-01-07 23:36:03 +0100

    Always use Oregon(c) protocol

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,29 @@
 /*
- * This file is part of the TinySensor distribution (https://github.com/arcadien/TinySensor)
- *
- * Copyright (c) 2019 Aurélien Labrosse
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+* This file is part of the TinySensor distribution
+* (https://github.com/arcadien/TinySensor)
+*
+* Copyright (c) 2019 Aurélien Labrosse
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, version 3.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <TinySensor.h>
 #include <config.h>
 
-#include <avr/io.h>
-#include <avr/wdt.h>
-#include <avr/sleep.h>
 #include <avr/interrupt.h>
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
 #include <string.h>
 
 #if defined(USE_OREGON)
@@ -35,37 +36,34 @@ Oregon oregon;
 BME280 bmX280;
 #endif
 
+void UseLessPowerAsPossible() {
+	//
+	//// unused pins are input + pullup, as stated
+	//// in Atmel's doc "AVR4013: picoPower Basics"
+	//
 
-void UseLessPowerAsPossible()
-{
-		//
-		//// unused pins are input + pullup, as stated
-		//// in Atmel's doc "AVR4013: picoPower Basics"
-		//
+	// all pins as input to avoid power draw
+	DDRA = 0;
+	DDRB = 0;
 
-		// all pins as input to avoid power draw
-		DDRA = 0;
-		DDRB = 0;
+	// pull-up all unused pins by default
+	//
+	PORTA |= 0b01111001;
+	PORTB |= 0b11111100;
 
-		// pull-up all unused pins by default
-		//
-		PORTA |= 0b01111001;
-		PORTB |= 0b11111100;
+	PRR &= ~_BV(PRTIM1);    // no timer1
+	PRR &= ~_BV(PRADC);     // no adc
+	ADCSRA &= ~(1 << ADEN); // p. 146
 
-		PRR &= ~_BV(PRTIM1);    // no timer1
-		PRR &= ~_BV(PRADC);     // no adc
-		ADCSRA &= ~(1 << ADEN); // p. 146
+	ACSR |= (1 << ACD); // no analog comparator (p. 129)
 
-		ACSR |= (1 << ACD); // no analog comparator (p. 129)
+	// deactivate brownout detection during sleep (p.36)
+	MCUCR |= (1 << BODS) | (1 << BODSE);
+	MCUCR |= (1 << BODS);
+	MCUSR &= ~(1 << BODSE);
 
-		// deactivate brownout detection during sleep (p.36)
-		MCUCR |= (1 << BODS) | (1 << BODSE);
-		MCUCR |= (1 << BODS);
-		MCUSR &= ~(1 << BODSE);
-
-		// don't use ADC buffers (p. 131)
-		DIDR0 |= (1 << ADC2D) | (1 << ADC1D;)
-	
+	// don't use ADC buffers (p. 131)
+	DIDR0 |= (1 << ADC2D) | (1 << ADC1D);
 }
 
 /*
@@ -92,7 +90,7 @@ ISR(WATCHDOG_vect) {
 
 void setup() {
 	wdt_disable();
-	
+
 	UseLessPowerAsPossible();
 
 	secondCounter = 0;
@@ -115,11 +113,9 @@ int avr_main(void) {
 
 	setup();
 
-	#if defined(USE_OREGON)
 	oregon.setType(oregon._oregonMessageBuffer, OREGON_TYPE);
 	oregon.setChannel(oregon._oregonMessageBuffer, Oregon::Channel::ONE);
 	oregon.setId(oregon._oregonMessageBuffer, OREGON_ID);
-	#endif
 
 	#if defined(USE_BME280) || defined(USE_BMP280)
 	bmX280.setI2CAddress(0x76);
@@ -127,7 +123,6 @@ int avr_main(void) {
 	bmX280.setStandbyTime(5);
 	#endif
 
-	#if defined(USE_OREGON)
 	// Buffer for Oregon message
 	#if OREGON_MODE == MODE_0
 	uint8_t _lastOregonMessageBuffer[8] = {};
@@ -138,11 +133,10 @@ int avr_main(void) {
 	#else
 	#error mode unknown
 	#endif
-	#endif
 
 	while (1) {
 
-		#if defined(USE_OREGON) && (defined(USE_BME280) || defined(USE_BMP280))
+		#if (defined(USE_BME280) || defined(USE_BMP280))
 		oregon.setBatteryLevel(oregon._oregonMessageBuffer, 1);
 		oregon.setTemperature(oregon._oregonMessageBuffer, bmX280.readTempC());
 
@@ -172,7 +166,6 @@ int avr_main(void) {
 		#endif
 
 		bool shouldEmitData = false;
-		#if defined(USE_OREGON)
 
 		#if (OREGON_MODE == MODE_0)
 		// temp update?
@@ -252,7 +245,6 @@ int avr_main(void) {
 		// led off
 		PORTB &= ~_BV(LED_PIN);
 
-		#endif
 		sleep(SLEEP_TIME_IN_SECONDS);
 		secondCounter += SLEEP_TIME_IN_SECONDS;
 	}
@@ -281,7 +273,7 @@ void sleep(uint8_t s) {
 		uint8_t portb_backup = PORTB;
 		uint8_t ddra_backup = DDRA;
 		uint8_t ddrb_backup = DDRB;
-		
+
 		PRR |= (1 << PRUSI);
 		PRR |= (1 << PRTIM0);
 		PRR |= (1 << PRTIM1);
@@ -292,7 +284,7 @@ void sleep(uint8_t s) {
 
 		// pull-up all unused pins by default
 		PORTA |= 0b01110000;
-		
+
 		// pullup reset only
 		// ( only 4 lasts are available - p. 67)
 		PORTB |= 0b00001000;
