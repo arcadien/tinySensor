909591baf1075a38ea9e70a17ae81029afa62114
Aurelien Labrosse <aurelien@skima.fr>
2020-01-20 11:23:32 +0100

    chore: Allow custom parameters

--- init_avr.sh
+++ init_avr.sh
@@ -1,1 +1,1 @@
-cmake .. -DAVR_MCU=attiny84a -DCMAKE_TOOLCHAIN_FILE=../third_party/cmake-avr/generic-gcc-avr.cmake
+cmake .. -DAVR_MCU=attiny84a -DCMAKE_TOOLCHAIN_FILE=../third_party/cmake-avr/generic-gcc-avr.cmake $@
