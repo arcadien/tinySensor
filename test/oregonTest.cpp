611365c3dceeaa7f24c01b20de2a8de4f2bbd43f
Aurelien Labrosse <aurelien@skima.fr>
2020-11-06 19:07:03 +0100

    test: expect library to use Manchester encoding

--- test/oregonTest.cpp
+++ test/oregonTest.cpp
@@ -1,5 +1,6 @@
 /*
- * This file is part of the TinySensor distribution (https://github.com/arcadien/TinySensor)
+ * This file is part of the TinySensor distribution
+ * (https://github.com/arcadien/TinySensor)
  *
  * Copyright (c) 2019 Aurélien Labrosse
  *
@@ -16,40 +17,188 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <string>
 #include <unity.h>
-#include <protocol/Oregon.h>
-void setUp(void)
-{
+#include <utility>
+#include <vector>
+
+namespace {
+namespace Oregon {
+
+inline bool BitRead(uint8_t value, uint8_t bit) {
+  return (((value) >> (bit)) & 0x01);
+}
+/*
+inline void sendZero(void) {
+  txPinHigh();
+  _delay_us(TIME);
+  txPinLow();
+  _delay_us(TWOTIME);
+  txPinHigh();
+  _delay_us(TIME);
 }
 
-void tearDown(void)
-{
+inline void sendOne(void) {
+  txPinLow();
+  _delay_us(TIME);
+  txPinHigh();
+  _delay_us(TWOTIME);
+  txPinLow();
+  _delay_us(TIME);
 }
 
-void Expect_Oregon_Buffers_to_be_correctly_sized_in_mode0()
-{
-    Oregon<MODE_0> oregon;
-    TEST_ASSERT_EQUAL(Oregon<MODE_0>::BUFF_SIZE[MODE_0], sizeof(oregon._oregonMessageBuffer));
+
+*/
+class Hal {
+public:
+  virtual void DelayUs(uint16_t duration) const = 0;
+  virtual void TxPinHigh() const = 0;
+  virtual void TxPinLow() const = 0;
+};
+/*
+ * This class propose implementation of Oregon v3 messages
+ * for environment sensors able to report temperature, humidity and pressure.
+ * Given the data exposed, specific device will be emulated.
+ * - Temperature only : THN132N sensor, type ID 0xEC40
+ */
+class v3 {
+
+public:
+  /*!
+   *
+   * @param hal the component responsible in nibble emission (delay and pin
+   * states)
+   * @param period clock period for manchester encoding
+   */
+  v3(Hal const &hal, uint16_t period)
+      : _hal(hal), _period(period), _halfPeriod(period / 2) {}
+
+  void SendData(uint8_t *data, uint8_t size) {
+    for (uint8_t i = 0; i < size; ++i) {
+      SendQuarterLSB(data[i]);
+      SendQuarterMSB(data[i]);
+    }
+  }
+
+private:
+  void SendQuarterMSB(const uint8_t data) {
+    (BitRead(data, 4)) ? SendOne() : SendZero();
+    (BitRead(data, 5)) ? SendOne() : SendZero();
+    (BitRead(data, 6)) ? SendOne() : SendZero();
+    (BitRead(data, 7)) ? SendOne() : SendZero();
+  }
+
+  void SendQuarterLSB(const uint8_t data) {
+    (BitRead(data, 0)) ? SendOne() : SendZero();
+    (BitRead(data, 1)) ? SendOne() : SendZero();
+    (BitRead(data, 2)) ? SendOne() : SendZero();
+    (BitRead(data, 3)) ? SendOne() : SendZero();
+  }
+
+  /*!
+   * off to on at the middle of a clock period
+   */
+  void SendZero(void) {
+    _hal.TxPinHigh();
+    _hal.DelayUs(_halfPeriod);
+    _hal.TxPinLow();
+    _hal.DelayUs(_period);
+    _hal.TxPinHigh();
+    _hal.DelayUs(_halfPeriod);
+  }
+
+  /*!
+   * on to to off at the middle of a clock period
+   */
+  void SendOne(void) {
+    _hal.TxPinLow();
+    _hal.DelayUs(_halfPeriod);
+    _hal.TxPinHigh();
+    _hal.DelayUs(_period);
+    _hal.TxPinLow();
+    _hal.DelayUs(_halfPeriod);
+  }
+  uint16_t const _period;
+  uint16_t const _halfPeriod;
+  Hal const &_hal;
+};
+} // namespace Oregon
+
+class TestHal : public Oregon::Hal {
+public:
+  void DelayUs(uint16_t duration) const override { Orders.push_back('D'); }
+  void TxPinHigh() const override { Orders.push_back('H'); }
+  void TxPinLow() const override { Orders.push_back('L'); }
+
+  mutable std::vector<unsigned char> Orders;
+};
+
+} // namespace
+/*
+ * This testsuite is based on the document
+ * "434MHz RF Protocol Descriptions for Wireless Weather Sensors,
+ *  Oregon Scientific, AcuRite, Ambient Weather
+ *  October 2015"
+ *  A copy of the document should be provided along the implementation
+ */
+void setUp(void) {}
+
+void tearDown(void) {}
+void Expect_bitread_to_read_each_nibble_separately() {
+
+  std::string message = "";
+
+  uint8_t value = 0;
+
+  for (uint8_t bitIndex = 0; bitIndex < 8; ++bitIndex) {
+    message = "Failed with value==0 for bit " + std::to_string(bitIndex);
+    TEST_ASSERT_FALSE_MESSAGE(Oregon::BitRead(value, bitIndex),
+                              message.c_str());
+  }
+
+  value = 1; // 0b00000001
+  for (uint8_t positionOfOne = 0; positionOfOne < 8; ++positionOfOne) {
+
+    for (uint8_t bitIndex = 0; bitIndex < 8; ++bitIndex) {
+      message = "Failed when 1 is at index " + std::to_string(positionOfOne) +
+                ", for bit " + std::to_string(bitIndex);
+      if (positionOfOne == bitIndex) {
+        TEST_ASSERT_TRUE_MESSAGE(Oregon::BitRead(value, bitIndex),
+                                 message.c_str());
+      } else {
+        TEST_ASSERT_FALSE_MESSAGE(Oregon::BitRead(value, bitIndex),
+                                  message.c_str());
+      }
+    }
+    value <<= 1; // 0b00000010
+  }
 }
 
-void Expect_Oregon_Buffers_to_be_correctly_sized_in_mode1()
-{
-    Oregon<MODE_1> oregon1;
-    TEST_ASSERT_EQUAL(Oregon<MODE_1>::BUFF_SIZE[MODE_1], sizeof(oregon1._oregonMessageBuffer));
+void Expect_messages_to_be_manchester_encoded() {
+  uint8_t value = 0b00000000;
+
+  TestHal testHal;
+  Oregon::v3 v3(std::move(testHal), 512);
+
+  // Send one byte
+  v3.SendData(&value, 1);
+
+  // Sending one nibble is 3 pin state changes
+  // and 3 delays, to have the pin change at
+  // middle of Manchester period. That is,
+  // 6 orders are needed to send a nibble.
+  uint16_t ordersCountNeededForANibble = 6;
+  uint16_t actualSize = static_cast<uint16_t>(testHal.Orders.size());
+  TEST_ASSERT_EQUAL_INT16_MESSAGE(ordersCountNeededForANibble * 8, actualSize,
+                                  "Sending a byte must lead to 6x8 orders");
+
+  // std::vector<unsigned char> expectedOne({H,L,H});
 }
 
-void Expect_Oregon_Buffers_to_be_correctly_sized_in_mode2()
-{
-    Oregon<MODE_2> oregon2;
-    TEST_ASSERT_EQUAL(Oregon<MODE_2>::BUFF_SIZE[MODE_2], sizeof(oregon2._oregonMessageBuffer));
-}
+int main(int, char **) {
+  UNITY_BEGIN();
+  RUN_TEST(Expect_bitread_to_read_each_nibble_separately);
+  RUN_TEST(Expect_messages_to_be_manchester_encoded);
 
-int main(int, char **)
-{
-    UNITY_BEGIN();
-    RUN_TEST(Expect_Oregon_Buffers_to_be_correctly_sized_in_mode0);
-    RUN_TEST(Expect_Oregon_Buffers_to_be_correctly_sized_in_mode1);
-    RUN_TEST(Expect_Oregon_Buffers_to_be_correctly_sized_in_mode2);
-
-    return UNITY_END();
+  return UNITY_END();
 }
