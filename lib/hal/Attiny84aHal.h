1da5054a28970708c293ff652d35f4142454b4fb
Aurelien Labrosse <aurelien@skima.fr>
2023-08-26 15:41:24 +0000

    chore: temp cleanup

--- lib/hal/Attiny84aHal.h
+++ lib/hal/Attiny84aHal.h
@@ -1,42 +1,39 @@
 #pragma once
 
-#ifndef __AVR_ATtiny84A_HAL__
-#define __AVR_ATtiny84A_HAL__
-#endif
-
 #include <Hal.h>
-#include <stdint.h>
+#include <util/delay.h>
 
-#if defined(USE_I2C)
-#include <TinyI2CMaster.h>
-#endif
-class Attiny84aHal : public Hal
+/*
+* 1-wire interface is on PA3
+* Power for radio is on PA2 (sensor_vcc)
+* LED pin is PB1
+* PWM pin is PA7
+* SDA pin is PA6
+* SCL pin is PA4
+* MISO pin is PA5
+* TX radio pin is PB0
+* BAT sensor pin is PA1
+* analog sensor pin is PA0
+*/
+
+#define LED_PIN PB1
+#define PWM_PIN PA7
+#define TX_RADIO_PIN PB0
+#define SENSOR_VCC PA2
+#define BAT_SENSOR_PIN PA1
+
+
+class ATtiny84aHal : public Hal
 {
-public:
-  Attiny84aHal();
+    public:
 
-  uint16_t GetVccVoltageMv(void) override;
-  uint16_t GetBatteryVoltageMv() override;
+    // Radio related
+    void LedOn() const override;
 
-  void LedOn() override;
-  void LedOff() override;
+    // Radio related
+    void RadioGoLow() const override;
+    void RadioGoHigh() const override;
 
-  void RadioGoLow() override;
-  void RadioGoHigh() override;
-
-  void Hibernate(uint8_t seconds) override;
-  void PowerOnSensors() override;
-  void PowerOffSensors() override;
-
-  void Delay1s() override;
-  void Delay30ms() override;
-  void Delay512Us() override;
-  void Delay1024Us() override;
-
-  void InitI2C(void) override
-  {
-#if defined(USE_I2C)
-    TinyI2C.init();
-#endif
-  }
 };
+
+#define OREGON_DELAY_US(x) _delay_us(x);
