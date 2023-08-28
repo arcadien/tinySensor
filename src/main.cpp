8883d045955c8cee887bdee4330034968fcc08f8
Aurelien Labrosse <aurelien@skima.fr>
2023-08-28 06:58:32 +0000

    chore: slight changes with leds and constants

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,31 @@
 /*
-* This file is part of the TinySensor distribution
-* (https://github.com/arcadien/TinySensor)
-*
-* Copyright (c) 2019 Aurelien Labrosse
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, version 3.
-*
-* This program is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of the TinySensor distribution
+ * (https://github.com/arcadien/TinySensor)
+ *
+ * Copyright (c) 2019 Aurelien Labrosse
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <config.h>
+
+#include <BMx280.h>
+#include <x10rf.h>
+
+#if defined(AVR)
+#include <ds18b20.h>
+#endif
+
 #include <Oregon_v3.h>
 
 #if defined(__AVR_ATtiny84__)
@@ -28,29 +36,18 @@ Attiny84aHal hal;
 TestHal_ hal;
 #endif
 
-
 #if defined(USE_BME280) or defined(USE_BMP280)
-#include <BMx280.h>
 BMx280 bmx280(&hal);
 #endif
 
 #if defined(VOLTAGE_X10_SENSOR_ID)
-#include <x10.h>
-x10 voltageEmitter;
+x10rf voltageEmitter(&hal, 5);
 #endif
 
-/*
-#if defined(USE_DS18B20)
-#include <ds18b20.h>
-#endif
-*/
-
-
-int main(void)
-{
+int main(void) {
   OregonV3 oregon(&hal);
-  oregon.SetChannel(OREGON_CHANNEL);
-  oregon.SetRollingCode(OREGON_ID);
+  oregon.SetChannel(SENSOR_CHANNEL);
+  oregon.SetRollingCode(SENSOR_ID);
 
   /*
    * The firmware force emission of
@@ -61,12 +58,11 @@ int main(void)
    */
   volatile uint16_t secondCounter = 901;
 
-  while (1)
-  {
+  while (1) {
 
     hal.PowerOnSensors();
     hal.Delay30ms();
-    bool batteryIsLow = false;
+    hal.LedOn();
 
 #if defined(USE_I2C)
     hal.InitI2C();
@@ -87,53 +83,32 @@ int main(void)
 
     // Delay (sensor needs time to perform conversion)
     hal.Delay1s();
-    
+
     // Read temperature (without ROM matching)
     int16_t temperature = 0;
     auto readStatus =
         ds18b20read(&PORTA, &DDRA, &PINA, (1 << 3), nullptr, &temperature);
-    if (readStatus == DS18B20_ERROR_OK)
-    {
+    if (readStatus == DS18B20_ERROR_OK) {
       oregon.SetTemperature(temperature / 16);
     }
 
 #endif
 
-    bool shouldEmitVoltage = false;
+    uint32_t voltageInMv = (uint32_t)hal.GetBatteryVoltageMv();
+    oregon.SetBatteryLow((voltageInMv < LOW_BATTERY_VOLTAGE));
 
     // absolute counter for emission ~ each 15 minutes
-    if (secondCounter > 900)
-    {
+    if (secondCounter > 900) {
       secondCounter = 0;
-      shouldEmitVoltage = true;
-    }
-
-    if (shouldEmitVoltage)
-    {
-
-      uint16_t voltageInMv = hal.GetBatteryVoltageMv();
-      batteryIsLow = (voltageInMv < LOW_BATTERY_VOLTAGE);
-
-#if defined(VOLTAGE_X10_SENSOR_ID)
-      voltageEmitter.RFXmeter(VOLTAGE_X10_SENSOR_ID, 0, voltageInMv);
+      voltageEmitter.RFXmeter(VOLTAGE_X10_SENSOR_ID, 0x00, voltageInMv);
       hal.Delay30ms();
-#endif
     }
-
-    if (batteryIsLow)
-    {
-      oregon.SetBatteryLow();
-    }
-
-    hal.LedOn();
     oregon.Send();
-    hal.LedOff();
     hal.Delay30ms();
-    hal.LedOn();
     oregon.Send();
+
     hal.LedOff();
     hal.PowerOffSensors();
-
     hal.Hibernate((uint8_t)SLEEP_TIME_IN_SECONDS);
     secondCounter += SLEEP_TIME_IN_SECONDS;
   }
