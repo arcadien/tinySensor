fa925ed3636b7b4639706d9f27bfaddaa4404161
Aurelien Labrosse <aurelien@skima.fr>
2023-10-01 18:37:38 +0200

    feat: enhance BMx280 interface

--- include/BMx280.h
+++ include/BMx280.h
@@ -4,35 +4,29 @@
 #if defined(AVR)
 #include <SparkFunBME280.h>
 #endif
-class BMx280
-{
+class BMx280 {
 
 #if defined(AVR)
   BME280 _bmX280;
 
 public:
-  BMx280(Hal *hal) : hal(hal)
-  {
-    _bmX280.setI2CAddress(0x76);
-  }
+  BMx280(Hal *hal) : hal(hal) { _bmX280.setI2CAddress(0x76); }
 
-  void Begin()
-  {
-    _bmX280.beginI2C();
-  }
+  void Begin() { _bmX280.beginI2C(); }
 
   float GetTemperature() { return _bmX280.readTempC(); }
   float GetPressure() { return _bmX280.readFloatPressure(); }
   float GetHumidity() { return _bmX280.readFloatHumidity(); }
+  bool IsMeasuring() { return _bmX280.isMeasuring(); }
+  void Shutdown() { return _bmX280.setMode(MODE_SLEEP); }
+
 #else
 public:
   void Begin() {}
   float GetTemperature() { return 0; }
   float GetPressure() { return 0; }
   float GetHumidity() { return 0; }
-  BMx280(Hal *hal) : hal(hal)
-  {
-  }
+  BMx280(Hal *hal) : hal(hal) {}
 #endif
 
 private:
